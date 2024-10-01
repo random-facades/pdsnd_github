382d00d137e2ebbce3fe10afd5ba2cbf4945cedd
Random Facades <random.facades@gmail.com>
2024-10-01 13:54:59 -0400

    refactor: Rename df to dataFrame

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -38,14 +38,14 @@ def load_data(city, month, day):
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+        dataFrame - Pandas DataFrame containing city data filtered by month and day
     """
 
 
-    return df
+    return dataFrame
 
 
-def time_stats(df):
+def time_stats(dataFrame):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
@@ -64,7 +64,7 @@ def time_stats(df):
     print('-'*40)
 
 
-def station_stats(df):
+def station_stats(dataFrame):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
@@ -83,7 +83,7 @@ def station_stats(df):
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def trip_duration_stats(dataFrame):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
@@ -99,7 +99,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 
-def user_stats(df):
+def user_stats(dataFrame):
     """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
@@ -121,12 +121,12 @@ def user_stats(df):
 def main():
     while True:
         city, month, day = get_filters()
-        df = load_data(city, month, day)
+        dataFrame = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        time_stats(dataFrame)
+        station_stats(dataFrame)
+        trip_duration_stats(dataFrame)
+        user_stats(dataFrame)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
